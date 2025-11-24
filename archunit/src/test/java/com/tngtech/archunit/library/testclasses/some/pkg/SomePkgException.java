255cc41d981387abf9d825e3d00f37d45ce33781
Jens Bannmann <jens.b@web.de>
2025-11-24 18:51:20 +0100

    remove unused members from exception

--- archunit/src/test/java/com/tngtech/archunit/library/testclasses/some/pkg/SomePkgException.java
+++ archunit/src/test/java/com/tngtech/archunit/library/testclasses/some/pkg/SomePkgException.java
@@ -1,13 +1,4 @@
 package com.tngtech.archunit.library.testclasses.some.pkg;
 
 public class SomePkgException extends RuntimeException {
-    private final int baz;
-
-    public SomePkgException(int baz) {
-        this.baz = baz;
-    }
-
-    public int getBaz() {
-        return baz;
-    }
 }
